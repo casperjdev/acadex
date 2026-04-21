c7a12b8f41e89dd147b39d0d220159105372e489
casperjdev <casperj.dev@gmail.com>
2026-04-21 10:02:15 +0200

    test

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+useHead({
+	link: [
+		{
+			rel: 'preconnect',
+			href: 'https://fonts.googleapis.com',
+		},
+		{
+			rel: 'preconnect',
+			href: 'https://fonts.gstatic.com',
+			crossorigin: 'anonymous',
+		},
+		{
+			rel: 'stylesheet',
+			href: 'https://fonts.googleapis.com/css2?family=Google+Sans+Flex:opsz,wght@6..144,1..1000&display=swap',
+		},
+	],
+});
+</script>
+
 <template>
 	<slot />
 </template>
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+	title: 'acadex',
+});
+</script>
 
 <template>
 	<h1>hello</h1>
